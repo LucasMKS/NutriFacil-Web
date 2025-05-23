fe2270e90163d7d650f673c4c502a976f925e262
LucasMKS <lucas.mar17ques@gmail.com>
2025-05-23 11:18:33 -0300

    feat: adicionar funções para cálculos de IMC, TMB, água e recomendações de dieta

---
+++ src/lib/api.tsx
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8080/api";
+
+export const calcularImc = async (peso: number, altura: number) => {
+  const resp = await axios.post(`${API_URL}/imc/calcular`, { peso, altura });
+  return resp.data;
+};
+
+export const calcularTmb = async (
+  peso: number,
+  altura: number,
+  idade: number,
+  sexo: string
+) => {
+  const resp = await axios.post(`${API_URL}/tmb/calcular`, {
+    peso,
+    altura,
+    idade,
+    sexo,
+  });
+  return resp.data;
+};
+
+export const calcularAgua = async (peso: number) => {
+  const resp = await axios.post(`${API_URL}/agua/calcular`, { peso });
+  return resp.data;
+};
+
+export const recomendarDieta = async (
+  tipoDieta: string,
+  restricoes: string[]
+) => {
+  const resp = await axios.post(`${API_URL}/dieta/recomendar`, {
+    tipoDieta,
+    restricoes,
+  });
+  return resp.data;
+};
