8624ac805d0df8bd96159837a3c9c3277a96321e
LucasMKS <lucas.mar17ques@gmail.com>
2025-05-23 13:29:36 -0300

    feat: adicionar suporte a estilos escuros e bordas no componente Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,14 +19,14 @@ const menu = [
 
 export default function Navbar({ active }: { active?: string }) {
   return (
-    <nav className="flex gap-4 py-4 px-8 bg-white shadow-md items-center justify-center sticky top-0 z-50">
+    <nav className="flex gap-4 py-4 px-8 bg-white shadow-md items-center justify-center sticky top-0 z-50 dark:bg-verde-escuro border-b border-verde-mais dark:text-white">
       {menu.map((item) => (
         <Link
           key={item.href}
           href={item.href}
           className={cn(
-            "flex items-center gap-2 px-3 py-2 rounded-lg font-medium transition hover:bg-gray-100",
-            active === item.href && "bg-gray-100 shadow"
+            "flex items-center gap-2 px-3 py-2 rounded-lg font-medium transition hover:bg-gray-100 dark:hover:bg-verde-mais/20",
+            active === item.href && ""
           )}
         >
           <item.icon className="w-5 h-5" />
